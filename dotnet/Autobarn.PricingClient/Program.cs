e6883b0fbd503273101a29be4ac2817d11b14d4e
Dylan Beattoie <dylan@dylanbeattie.net>
2022-05-31 13:35:33 +0200

    Working gRPC client/server using .NET

--- dotnet/Autobarn.PricingClient/Program.cs
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Autobarn.Messages;
+using Autobarn.PricingEngine;
+using EasyNetQ;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 
-namespace Autobarn.PricingClient
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Autobarn.PricingClient {
+    class Program {
+        private static readonly IConfigurationRoot config = ReadConfiguration();
+
+        static void Main(string[] args) {
+            using var channel = GrpcChannel.ForAddress(config["AutobarnPricingServerUrl"]);
+            var client = new Pricer.PricerClient(channel);
+            var amqp = config.GetConnectionString("AutobarnRabbitMqConnectionString");
+            using var bus = RabbitHutch.CreateBus(amqp);
+            var handler = MakeHandler(client);
+            bus.PubSub.Subscribe("Autobarn.PricingClient", handler);
+            Console.WriteLine("Autobarn.PricingClient ready! Listening for NewVehicleMessages...");
+            Console.ReadLine();
+        }
+
+        private static Func<NewVehicleMessage, Task> MakeHandler(Pricer.PricerClient client) {
+            return async message => {
+                Console.WriteLine(
+                    $"Getting price for {message.ManufacturerName} {message.ModelName} ({message.Color}, {message.Year}");
+                var priceRequest = new PriceRequest {
+                    Color = message.Color,
+                    Year = message.Year,
+                    Make = message.ManufacturerName,
+                    Model = message.ModelName
+                };
+                var priceReply = await client.GetPriceAsync(priceRequest);
+                Console.WriteLine($"Price: {priceReply.Price} {priceReply.CurrencyCode}");
+            };
+        }
+
+        private static IConfigurationRoot ReadConfiguration() {
+            var basePath = Directory.GetParent(AppContext.BaseDirectory).FullName;
+            return new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
         }
     }
 }
---
+++ grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+using System.Diagnostics;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7257");
+var client = new Greeter.GreeterClient(channel);
+var count = 1;
+var MAX = 10000;
+var sw = new Stopwatch();
+sw.Start();
+while(count < MAX) {
+    var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = $"NDC Copenhagen {count++}" }
+    );
+    // Console.WriteLine(reply.Message);
+}
+sw.Stop();
+Console.WriteLine($"gRPC did {MAX} requests in {sw.ElapsedMilliseconds}ms");
+
