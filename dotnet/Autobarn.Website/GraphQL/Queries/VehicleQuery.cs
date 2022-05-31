192edf5f1dd887bea47a631c3c7029048dc5d0ca
Dylan Beattoie <dylan@dylanbeattie.net>
2022-05-31 11:17:46 +0200

    Working GraphQL queries with enumerated types

--- dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -33,10 +33,38 @@ namespace Autobarn.Website.GraphQL.Queries {
                 new QueryArguments(MakeNonNullStringArgument("color",
                     "What color cars do you want?")),
                     resolve: GetVehiclesByColor);
+            Field<ListGraphType<VehicleGraphType>>(
+                "VehiclesByYear",
+                "Return all vehicles of a specific year",
+                new QueryArguments(MakeNonNullIntArgument("year", "What cars do you want from a specific year?")),
+                resolve: GetVehiclesByYear);
         }
+
+        private QueryArgument MakeNonNullIntArgument(string name, string description) {
+            return new QueryArgument<NonNullGraphType<IntGraphType>> {
+                Name = name, Description = description
+            };
+        }
+
+        private object GetVehiclesByYear(IResolveFieldContext<object> context) {
+            //var year = context.GetArgument<int>("year");
+            //return db.ListVehicles().Where(v => v.Year == year);
+
+            // private object GetVehiclesByYear(IResolveFieldContext<object> context) {
+            var year = context.GetArgument<int>("year");
+            var op = context.GetArgument<string>("op");
+            op = op?.ToLowerInvariant();
+            return op switch {
+                "newer" => db.ListVehicles().Where(v => v.Year > year),
+                "older" => db.ListVehicles().Where(v => v.Year < year),
+                _ => db.ListVehicles().Where(v => v.Year == year)
+            };
+        }
+
 
         private object GetVehiclesByColor(IResolveFieldContext<object> context) {
             var color = context.GetArgument<string>("color");
+            if (color == "Brown") throw new InvalidOperationException("Brown cars are not allowed in GraphQL!");
             return db.ListVehicles().Where(v => v.Color.Contains(color));
         }
 
@@ -51,8 +79,38 @@ namespace Autobarn.Website.GraphQL.Queries {
             };
         }
 
+
         private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> context) {
             return db.ListVehicles();
+        }
+        private object GetVehiclesByYearV2(IResolveFieldContext<object> context) {
+            var year = context.GetArgument<int>("year");
+            var compare = context.GetArgument<CompareGraphType>("CompareType");
+            var resultCount = context.GetArgument<int?>("ResultCount");
+
+            return compare switch {
+                CompareGraphType.After => !resultCount.HasValue
+                    ? db.ListVehicles().Where(v => v.Year > year)
+                    : db.ListVehicles().Where(v => v.Year > year).Take(resultCount.Value),
+                CompareGraphType.Before => !resultCount.HasValue
+                    ? db.ListVehicles().Where(v => v.Year < year)
+                    : db.ListVehicles().Where(v => v.Year < year).Take(resultCount.Value),
+                _ => !resultCount.HasValue
+                    ? db.ListVehicles().Where(v => v.Year == year)
+                    : db.ListVehicles().Where(v => v.Year == year).Take(resultCount.Value)
+            };
+        }
+
+        private QueryArgument MakeEnumArgument(string name, string description) {
+            return new QueryArgument<NonNullGraphType<EnumerationGraphType<CompareGraphType>>> {
+                Name = name, Description = description
+            };
+        }
+
+        public enum CompareGraphType {
+            Exact,
+            Before,
+            After
         }
     }
 }
